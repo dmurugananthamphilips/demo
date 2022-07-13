1724f332b61bce0bcde2f8487264568001c77413
Brindha <Brindha33@gmail.com>
2022-07-13 16:29:37 +0530

    First Checkin for sample .net core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SampleForGitHubLearning
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World and GitHubLearners");
+        }
+    }
+}
+
